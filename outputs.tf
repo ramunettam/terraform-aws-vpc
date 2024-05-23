814fbf68da25d201ce0650abafe050834654225e
ramunettam <ramunettam@gmail.com>
2024-05-23 14:19:49 +0200

    vpc

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,22 @@
-output "azs"{
-      value= data.aws_availability_zones.available.names
+output "azs" {
+     value = data.aws_availability_zones.available.names
+ }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
   
-} 
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+  
+}
+
+output "database_subnet_id" {
+  value = aws_subnet.database.id
+  
+}
